96173308de842459ccbbe2d6ef4be35a5280d45b
verxirtam <d-kosuga@jcom.home.ne.jp>
2014-11-06 02:23:40 +0900

    初期コミット インタプリタでメッサーじ読み込みのテスト実装を行った。 まだ"一本道"で、読み込みながら実行する体で書いているので compositeっぽくなっていない。（今のところ枝を保持する必要がないため。） 未実装部分： ・MessageContextの実装(Socketの想定) ・個別のコマンドSetMDPやExecEpisodの実装 ・main()メソッドの実装

---
+++ src/InterpreterTest.java
@@ -0,0 +1,99 @@
+
+
+
+interface MessageContext
+{
+	String nextToken();
+	String skipToken(String skipString);
+	String skipReturn();
+	boolean hasNextToken();
+}
+
+interface MessageProcedure
+{
+	void parse(MessageContext context) throws Exception;
+}
+
+class Message implements MessageProcedure
+{
+	@Override
+	public void parse(MessageContext context) throws Exception
+	{
+		context.skipToken("MASSAGE_1.0");
+		context.skipReturn();
+		MessageBody mb=new MessageBody();
+		mb.parse(context);
+	}
+}
+
+class MessageBody implements MessageProcedure
+{
+
+	@Override
+	public void parse(MessageContext context) throws Exception
+	{
+		context.skipToken("EV3LineTracer_1.0");
+		context.skipReturn();
+		Command com=new Command();
+		com.parse(context);
+	}
+	
+}
+
+class Command implements MessageProcedure
+{
+
+	@Override
+	public void parse(MessageContext context) throws Exception
+	{
+		String commandstring = context.nextToken();
+		context.skipReturn();
+		
+		createCommand(commandstring).parse(context);
+		
+		
+	}
+	MessageProcedure createCommand(String commandstring) throws Exception
+	{
+		if(commandstring.equals("SetMDP"))
+		{
+			return new CommandSetMDP();
+		}
+		if(commandstring.equals("ExecEpisode"))
+		{
+			return new CommandExecEpisode();
+		}
+		throw new Exception(this.getClass().getName());
+	}
+}
+
+class CommandSetMDP implements MessageProcedure
+{
+
+	@Override
+	public void parse(MessageContext context) throws Exception
+	{
+		//コマンド毎の処理を実装する
+		
+	}
+	
+}
+class CommandExecEpisode implements MessageProcedure
+{
+
+	@Override
+	public void parse(MessageContext context) throws Exception
+	{
+		//コマンド毎の処理を実装する
+	}
+	
+}
+public class InterpreterTest
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
